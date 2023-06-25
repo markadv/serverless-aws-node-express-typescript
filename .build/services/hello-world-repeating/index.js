d69a33b3601eba7ce642e0f46b802589e0f3eeed
Mark Timothy Advento <95460588+markadv@users.noreply.github.com>
2023-06-26 07:13:50 +0800

    Remove build

--- .build/services/hello-world-repeating/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.run = void 0;
-const run = async (event, context) => {
-    const time = new Date();
-    console.log(`Your cron function "${context.functionName}" ran at ${time}`);
-};
-exports.run = run;
-//# sourceMappingURL=index.js.map
